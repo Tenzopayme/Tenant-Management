1b2dc96468ac71bfa6ec05c56ddb9a23c9705a14
abhikpaulcs20 <abhikpaulcs20@gmail.com>
2024-12-20 16:58:23 +0530

    Laka

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-2xl font-bold underline">pass hi </h1>;
+  return <h1 className="text-2xl font-bold underline">pass hi pass </h1>;
 }
 export default App;
