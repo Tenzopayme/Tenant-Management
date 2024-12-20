5a4e53928eb5e4a52faf27016a0533d297ee1833
abhikpaulcs20 <abhikpaulcs20@gmail.com>
2024-12-20 16:02:50 +0530

    Testing the value

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-2xl font-bold underline">Hello js </h1>;
 }
 
 export default App;
