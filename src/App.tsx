c4455ccf210f1f4aac79226445749c33c8c2878f
abhikpaulcs20 <abhikpaulcs20@gmail.com>
2024-12-20 16:12:43 +0530

    pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-2xl font-bold underline">Hello js </h1>;
+  return <h1 className="text-2xl font-bold underline">pass </h1>;
 }
 
 export default App;
